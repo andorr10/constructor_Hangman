cb7c4f48a752ce01296be50ef6a1b014d87aa6bd
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2017-11-05 12:20:35 -0600

    inquirer prompt

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var gameResult = false;
 
 console.log(RandWordInfo);
 var indexOfWord = 1;
-var newWord = new RandWordInfo(indexOfWord);
+var newWord = new module.RandWordInfo(indexOfWord);
 //console.log(newWord.word);
 //console.log(newWord.wordLength);
 newWord.printWordInfo();
@@ -16,35 +16,35 @@ newWord.printWordInfo();
 
 
 
-// function startGame(){
-// inquirer.prompt({
-//   type: "list",
-//   name: "usersChoice",
-//   message: "Do you want to play Game of Thrones Hang Man",
-//   choices: ["yes", "no"]
-// }).then(function(response){
-//   //console.log("You chose: " + response.usersChoice);
-//   if (response.usersChoice === "yes"){
-//     selectWord(randWord);
-//     console.log("word is: " + );
-//     playGame(magicWord.module);
-//   }
-//   else{
-//     console.log("guess you're too scared to play the game of thrones");
-//   }
-// });
-// }
-//
-// function playGame(passThroughWord){
-//   inquirer.prompt({
-//     type: "input",
-//     name: "guess",
-//     message: "Guess a letter"
-//   });
-// }
-//
-//
-// startGame();
+function startGame(){
+inquirer.prompt({
+  type: "list",
+  name: "usersChoice",
+  message: "Do you want to play Game of Thrones Hang Man",
+  choices: ["yes", "no"]
+}).then(function(response){
+  //console.log("You chose: " + response.usersChoice);
+  if (response.usersChoice === "yes"){
+    selectWord(randWord);
+    console.log("word is: " + );
+    playGame(magicWord.module);
+  }
+  else{
+    console.log("guess you're too scared to play the game of thrones");
+  }
+});
+}
+
+function playGame(passThroughWord){
+  inquirer.prompt({
+    type: "input",
+    name: "guess",
+    message: "Guess a letter"
+  });
+}
+
+
+startGame();
 
 
 exports.module = gameResult;
