2f7f3f431c2c0a7e94ba7d71c5645f14bfb5a8fb
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2017-11-05 08:48:28 -0600

    constructor works to create new word

--- game.js
+++ game.js
@@ -3,43 +3,48 @@ var RandWordInfo = require("./randWord.js");
 var results = require('./winLossCounter.js');
 var gameResult = false;
 
-var dorthraki = new RandWordInfo;
-//console.log(RandWordInfo.module);
+console.log(RandWordInfo);
+var indexOfWord = 1;
+var newWord = new RandWordInfo(indexOfWord);
+//console.log(newWord.word);
+//console.log(newWord.wordLength);
+newWord.printWordInfo();
+//newWord.createBlanks();
 
 
 
 
 
 
-function startGame(){
-inquirer.prompt({
-  type: "list",
-  name: "usersChoice",
-  message: "Do you want to play Game of Thrones Hang Man",
-  choices: ["yes", "no"]
-}).then(function(response){
-  //console.log("You chose: " + response.usersChoice);
-  if (response.usersChoice === "yes"){
-    selectWord(randWord);
-    console.log("word is: " + )
-    playGame(magicWord.module);
-  }
-  else{
-    console.log("guess you're too scared to play the game of thrones");
-  }
-});
-}
-
-function playGame(passThroughWord){
-  inquirer.prompt({
-    type: "input",
-    name: "guess",
-    message: "Guess a letter"
-  });
-}
-
-
-startGame();
+// function startGame(){
+// inquirer.prompt({
+//   type: "list",
+//   name: "usersChoice",
+//   message: "Do you want to play Game of Thrones Hang Man",
+//   choices: ["yes", "no"]
+// }).then(function(response){
+//   //console.log("You chose: " + response.usersChoice);
+//   if (response.usersChoice === "yes"){
+//     selectWord(randWord);
+//     console.log("word is: " + );
+//     playGame(magicWord.module);
+//   }
+//   else{
+//     console.log("guess you're too scared to play the game of thrones");
+//   }
+// });
+// }
+//
+// function playGame(passThroughWord){
+//   inquirer.prompt({
+//     type: "input",
+//     name: "guess",
+//     message: "Guess a letter"
+//   });
+// }
+//
+//
+// startGame();
 
 
 exports.module = gameResult;
--- randWord.js
+++ randWord.js
@@ -1,14 +1,49 @@
-var wordList = ["dothraki", "dragon", "westeros", "winter", "cersei", "whitewalker", "citadel", "lanister", "kingslanding", "baratheon", "direwolf", "wildling", "danaerys", "valerian", "hodor", "nymeria", "littlefinger", "lordbaelish", "imp", "sandsnakes"];
-var guessesLeft = 10;
-var randWord = wordList[Math.floor(Math.random() * wordList.length)];
-var RandWordInfo = function(randWord){
-  word = randWord;
-  WordLength = randWord.length;
-};
-
-function selectWord(){
-  return randWord;
+// var wordList = ["dothraki", "dragon", "westeros", "winter", "cersei", "whitewalker", "citadel", "lanister", "kingslanding", "baratheon", "direwolf", "wildling", "danaerys", "valerian", "hodor", "nymeria", "littlefinger", "lordbaelish", "imp", "sandsnakes"];
+//var guessesLeft = 10;
+// var randWord = {
+// wordArray: ["dothraki", "dragon", "westeros", "winter", "cersei", "whitewalker", "citadel", "lanister", "kingslanding", "baratheon", "direwolf", "wildling", "danaerys", "valerian", "hodor", "nymeria", "littlefinger", "lordbaelish", "imp"],
+// randomWord: wordArray[Math.floor(Math.random() * wordArray.length)]
+// };
+// console.log(randWord);
+var wordList = ["dothraki", "dragon", "westeros", "winter", "cersei", "whitewalker", "citadel", "lanister", "kingslanding", "baratheon", "direwolf", "wildling", "danaerys", "valerian", "hodor", "nymeria", "littlefinger", "lordbaelish", "imp"];
+//var indexOfWord = Math.floor(Math.random() * wordList.length);
+var indexOfWord = 1;
+function RandWordInfo(indexOfWord){
+  //console.log(indexOfWord);
+  this.word = wordList[indexOfWord];
+  this.wordLength = this.word.length;
+  this.printWordInfo = function(){
+    console.log("word: " + this.word);
+    console.log("Length of " + this.word + "is " + this.wordLength);
+    console.log("___________________");
+  };
+  this.guessesLeft = 8;
+  this.incorrectGuesses = [];
+  this.createBlanks = function(){
+    console.log(this.word + " function works!");
+    var emptyWord = "";
+        for(var i = 0; i< this.wordLength; i++){
+          emptyWord = emptyWord + "_ ";
+        }
+    console.log(emptyWord);
+  };
+  this.replaceBlanks = function(){
+    
+  };
 }
+
+
+//console.log(RandWordInfo);
+//var newWord = new RandWordInfo(indexOfWord);
+//console.log(newWord.word);
+//console.log(newWord.wordLength);
+//newWord.printWordInfo();
+//newWord.createBlanks();
+
+
+// function selectWord(){
+//   return randWord;
+// }
 // var RandWordInfo = function(input){
 //   this.wordsArray = ["dothraki", "dragon", "westeros", "winter", "cersei", "whitewalker", "citadel", "lanister", "kingslanding", "baratheon", "direwolf", "wildling", "danaerys", "valerian", "hodor", "nymeria", "littlefinger", "lordbaelish", "imp", "sandsankes"];
 //   this.wordSelected = this.wordsArray[Math.floor(Math.random() * this.wordsArray.length)];
